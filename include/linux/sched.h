23da84670096a384e3ba4520570d245d79f3efc1
Dr.HSD <socialdoctor555@gmail.com>
2026-07-26 21:38:23 +0530

    Add susfs_last_fake_mnt_id and susfs_task_state fields

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1510,7 +1510,11 @@ struct task_struct {
 #else
 	ANDROID_KABI_RESERVE(7);
 #endif
-	ANDROID_KABI_RESERVE(8);
+#ifdef CONFIG_KSU_SUSFS
+    ANDROID_KABI_USE(8, u64 susfs_last_fake_mnt_id);
+#else
+    ANDROID_KABI_RESERVE(8);
+#endif
 
 #ifdef CONFIG_ANDROID_SIMPLE_LMK
 	struct task_struct		*simple_lmk_next;
@@ -1519,6 +1523,13 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
+#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
+	u64 susfs_task_state;
+#endif
+
+#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
+	u64 susfs_last_fake_mnt_id;
+#endif
 	randomized_struct_fields_end
 
 	/* CPU-specific state of this task: */
