21731357983123d830e08ee9a6ee97af72e7b063
Dr.HSD <socialdoctor555@gmail.com>
2026-07-26 21:30:52 +0530

    Add conditional inclusion of susfs_def.h

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -19,6 +19,9 @@
 #include <linux/shmem_fs.h>
 #include <linux/uaccess.h>
 #include <linux/pkeys.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+#include <linux/susfs_def.h>
+#endif
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
