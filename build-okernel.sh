d976950f7f1b043e2d069cecfeedf99fa74659a1
Dr.HSD <socialdoctor555@gmail.com>
2026-07-30 18:25:09 +0530

    Remove KBUILD environment variable exports
    Removed environment variable exports for build user, host, and timestamp.

--- build-okernel.sh
+++ build-okernel.sh
@@ -5,9 +5,6 @@
 
 set -euo pipefail
 
-export KBUILD_BUILD_USER="O-Kernel"
-export KBUILD_BUILD_HOST="umi-kernel"
-export KBUILD_BUILD_TIMESTAMP="$(date)"
 export ARCH=arm64
 export CC=/path/to/clang18/bin/clang
 
